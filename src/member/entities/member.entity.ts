a0fbbaede24bd398ca3ccad3350480e48d35181f
조유빈 (주)소프트웍스 <yubincho@softworks.co.kr>
2023-07-22 11:51:06 +0900

    add profile image

--- src/member/entities/member.entity.ts
+++ src/member/entities/member.entity.ts
@@ -2,6 +2,7 @@ import { CommonEntity } from '../../product/entities/common.entity';
 import { BeforeInsert, BeforeUpdate, Column, Entity } from 'typeorm';
 import { InternalServerErrorException } from '@nestjs/common';
 import * as bcrypt from 'bcryptjs';
+import * as gravatar from 'gravatar';
 
 @Entity()
 export class Member extends CommonEntity {
@@ -14,10 +15,22 @@ export class Member extends CommonEntity {
   @Column()
   public password: string;
 
+  @Column({ nullable: true })
+  public profileImg?: string;
+
   @BeforeInsert()
   @BeforeUpdate()
   async beforeFunction(): Promise<void> {
     try {
+      // profile image 자동 생성
+      this.profileImg = gravatar.url(this.email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm',
+        protocol: 'https',
+      });
+
+      // password 암호화
       const saltValue = await bcrypt.genSalt(10);
       const hashedPassword = await bcrypt.hash(this.password, saltValue);
       this.password = hashedPassword;
